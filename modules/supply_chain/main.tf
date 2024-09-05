cdf98c428d7cc7d9c3d8c09cbabaa6a04d9dda9f
Höhl <MHOEHL@controlware.de>
2024-09-05 17:08:03 +0200

    upgrade acr to premium sku

--- modules/supply_chain/main.tf
+++ modules/supply_chain/main.tf
@@ -2,8 +2,8 @@ resource "azurerm_container_registry" "default_acr" {
   name                = "acrRancherDemo"
   resource_group_name = var.resource_group_name
   location            = var.location
-  sku                 = "Standard"
-  admin_enabled       = false
+  sku                 = "Premium"
+  admin_enabled       = true
 }
 
 resource "azurerm_private_endpoint" "pep" {
